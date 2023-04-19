abcc4e529a8776a4f2cd283f764c2399cd3f9a5d
shayan vosoughi <shayanwqhw@gmail.com>
2023-04-20 03:21:32 +0430

    perf: :zap: better features content

--- src/shared/containers/components/Feature.tsx
+++ src/shared/containers/components/Feature.tsx
@@ -1,8 +1,7 @@
 import { Card, Typography } from "@mui/material";
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
-import PaidIcon from '@mui/icons-material/Paid';
-
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import CalculateIcon from '@mui/icons-material/Calculate';
 interface FeaturePropsInterface {
   featureData: {title:string, description:string}
   indexData:number
@@ -12,9 +11,9 @@ const Feature = (props: FeaturePropsInterface) => {
   const {featureData, indexData} = props
   return (
     <Card sx={{ padding: "1.5rem 1rem", minHeight: '270px', border: '1px solid rgb(0, 0, 0, 0.07)', margin: '1.5rem .6rem' }}>
-      {indexData === 0 && <SettingsIcon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
-      {indexData === 1 && <AccessTimeFilledIcon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
-      {indexData === 2 && <PaidIcon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
+      {indexData === 0 && <Inventory2Icon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
+      {indexData === 1 && <ManageAccountsIcon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
+      {indexData === 2 && <CalculateIcon sx={{fontSize: '2.2rem', color: '#7F5EFF'}} />}
       <Typography sx={{ fontSize: "1.5rem", margin: "1rem 0", fontWeight: 'bold' }}>{featureData.title}</Typography>
       <Typography sx={{ fontSize: "1.09rem", margin: "1rem 0" }} color={(t) => t.palette.grey[500]}>{featureData.description}</Typography>
     </Card>
